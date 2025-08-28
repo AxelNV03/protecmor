fd1cf048e196cc4852785e2f9142fdb54e3ca362
Nava <axelnava3105@gmail.com>
2025-08-28 18:44:15 -0500

    Login completo, junto a recovery password

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -56,16 +56,29 @@ class UserSeeder extends Seeder
         );
         $alumno->assignRole('alumno');
 
+        //5 Datos adicionales
+        $alumno2 = User::firstOrCreate(
+            ['email' => 'nsao220388@upemor.edu.mx'],
+            [
+                'name' => 'N test',
+                'password' => Hash::make('1234'),
+                'email_verified_at' => now(),
+            ]
+        );
+        $alumno2->assignRole('alumno');
+
         // Actualizar contraseñas si ya existían (opcional)
         $superAdmin->update(['password' => Hash::make('admin123')]);
         $admin->update(['password' => Hash::make('admin123')]);
         $profesor->update(['password' => Hash::make('profe123')]);
         $alumno->update(['password' => Hash::make('alumno123')]);
+        $alumno2->update(['password' => Hash::make('1234')]);
 
         $this->command->info('Usuarios de prueba verificados/creados exitosamente!');
         $this->command->info('Super Admin: sadmin@protecmor.com / admin123');
         $this->command->info('Admin: admin1@protecmor.com / admin123');
         $this->command->info('Profesor: profe1@protecmor.com / profe123');
         $this->command->info('Alumno: alumno1@protecmor.com / alumno123');
+        $this->command->info('Alumno: nsao220388@upemor.edu.mx / 1234');
     }
 }
